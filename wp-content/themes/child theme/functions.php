115b91cf2474154ab37d8eed4c47bb155f086d2a
Simar <simar@Gagandeeps-MacBook-Pro.local>
2018-07-16 20:30:04 -0400

    additional styling done in the style.css  and google fonts used

--- wp-content/themes/child theme/functions.php
+++ wp-content/themes/child theme/functions.php
@@ -1,19 +1,24 @@
-<? php
+<?php
+
 function wpb_add_google_fonts() {
-	
-	wp_enqueue_style('wpb-google-fonts',https://fonts.googleapis.com/css?family=PT+Serif|Work+Sans , false );
+
+	wp_enqueue_style ('wpb-google-fonts','https://fonts.googleapis.com/css?family=Gloria+Hallelujah|Shadows+Into+Light', false);
 }
 
-add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts');
+
+
+
+function my_theme_enqueue_style(){
+
+	$parent_style = 'twentysixteen-style'; //This is 'twentysixteen-style'for the Twenty Sixteen theme.
+
+	wp_enqueue_style($parent_style,
+	     get_template_directory_uri() . 'style.css');
+	wp_enqueue_style( 'child-style',
+         get_style_directory_uri() . 'style.css',
+         array( $parent_style ),
+         wp_get_theme()->get('Version')
+     );
 }
-function my_theme_enqueue_styles() {
-
-	$parent_style = 'twentysixteen-style'; 
-
-	wp_enqueue_style( $parent_style, get_template_directroty_uri(). '/styles.css');
-	wp_enqueue_style('child-style', get'_stylesheet_directry_uri(). '/styles.css', array( $parent_style), wp_get_theme()-->get('Version')
-	 );
-}
-
-add_action( 'wp_enqueue_scripts' , 'my_theme_enqueue_styles');
-?>
+add_action( 'wp_enqueue_scripts' , '
+   my_theme_enqueue_style');
